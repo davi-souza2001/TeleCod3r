4d281fa162bbee413584ff8f23597821a76776c7
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-19 08:03:46 -0300

    left bar complete

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,58 @@
 import { Header } from '../../components/Header'
+import Cod3rLogo from '../../../public/img/Cod3rLogo.png'
 import './home.css'
 
 export function Home() {
   return (
     <div className="contentGeralHome">
       <Header />
+      <div className="contentLeftBarHome">
+        <div className="contentMensageMembersHome">
+          <p>Membros online</p>
+        </div>
+        <div className="contentBoxUsersHome">
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+          <div className="boxUserHome">
+            <img src={Cod3rLogo} alt="User Image" />
+            <p>Davi Souza</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
